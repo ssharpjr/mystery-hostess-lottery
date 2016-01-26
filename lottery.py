f08423c9dc0eaa8d825cd5c4362f9123b4249ad3
Stacey Sharp <ssharpjr@gmail.com>
2016-01-25 22:57:19 -0500

    Created functions and added menu placeholders

--- lottery.py
+++ lottery.py
@@ -1,36 +1,83 @@
-# lottery.py
-# Author: Stacey Sharp (github.com/ssharpjr)
-# Version: 2016-01-24
-# Description: Gathers and tallys scores for a lottery drawing
+# -*- coding: utf-8 -*-
+'''
+ lottery.py
+ Author: Stacey Sharp (github.com/ssharpjr)
+ Version: 2016-01-24
+ Description: Gathers and tallys scores for a lottery drawing.
+'''
 
 import sys
 
 tickets = {}
+ticket_holders = {}
 counter = 1
 
-print("\nWelcome to the Lottery Maker 9000!")
-print("\nWhat would you like to do?")
-print("1. Enter a new player")
-print("2. See the existing player list")
-print("3. Pull the lottery!")
-print("Q. Exit the program")
-choice = input("\nChoose a menu option: ")
 
-if choice != '1':
-    print("\nNot ready yet, Goodbye :)")
+def notReady():
+    print("\nThis function is not ready yet.")
+    input("Press any key to continue...")
+
+
+def addPlayer(counter):
+    while True:
+        name = input("\nEnter the new player's name or 'Q' to quit: ")
+        if name == 'q':
+            main()
+        name = name.title()
+        entries = input("Enter %s's number of entries: " % name)
+        ticket_holders[name] = entries
+        print(name + " has " + entries + " tickets.\n")
+        entries = int(entries)
+        cEntries = counter + entries
+
+        for entry in range(counter, cEntries):
+            tickets[entry] = name
+            counter += 1
+
+        print(tickets)
+        print(ticket_holders)
+        print("\nTickets: " + str(entries))
+        print("Counter: %s" % counter)
+
+
+def playerList():
+    notReady()
+    main()
+
+
+def pullLottery():
+    notReady()
+    main()
+
+
+def exitProgram():
+    print("Goodbye")
     sys.exit()
 
-name = input("\nEnter the new player's name: ")
-name = name.title()
-entries = input("Enter %s's number of entries: " % name)
 
-print(name + " has " + entries + " tickets.\n")
-entries = int(entries)
+# Begin main
+def main():
+    print("\nWelcome to the Mystery Hostess Lottery Maker!")
+    print("\nWhat would you like to do?")
+    print("1. Enter a new player")
+    print("2. See the existing player list")
+    print("3. Pull the lottery!")
+    print("Q. Exit the program")
+    choice = input("\nChoose a menu option: ")
 
-for entry in range(counter, entries + 1):
-    tickets[entry] = name
-    counter = counter + 1
+    if choice == '1':
+        addPlayer(counter)
+    elif choice == '2':
+        playerList()
+    elif choice == '3':
+        pullLottery()
+    elif choice == 'q' or 'Q':
+        exitProgram()
+    else:
+        print("Not a valid option")
+        main()
 
-print(tickets)
-print("\nTickets: " + str(entries))
-print("Counter: %s" % counter)
+
+# Run main
+if __name__ == '__main__':
+    main()
